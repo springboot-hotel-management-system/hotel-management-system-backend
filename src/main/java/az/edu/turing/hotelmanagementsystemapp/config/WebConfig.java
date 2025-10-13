e54e8a2c7bfac4043411a8b162a824740b84e424
seyran7 <brsr888@gmail.com>
2025-10-13 22:14:07 +0400

    Updated WebConfig

--- src/main/java/az/edu/turing/hotelmanagementsystemapp/config/WebConfig.java
+++ src/main/java/az/edu/turing/hotelmanagementsystemapp/config/WebConfig.java
@@ -1,25 +1,16 @@
 package az.edu.turing.hotelmanagementsystemapp.config;
 
-import io.github.cdimascio.dotenv.Dotenv;
-import jakarta.annotation.PostConstruct;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
-
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*");
-            }
-        };
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET","POST","PUT","DELETE")
+                .allowCredentials(true);
     }
 }

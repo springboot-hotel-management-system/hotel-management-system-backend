8f2840ca96271dfefd9167305969e363a49f95eb
seyran7 <brsr888@gmail.com>
2025-10-15 22:13:39 +0400

    Added EnvConfig

--- src/main/java/az/edu/turing/hotelmanagementsystemapp/HotelManagementSystemApplication.java
+++ src/main/java/az/edu/turing/hotelmanagementsystemapp/HotelManagementSystemApplication.java
@@ -8,12 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HotelManagementSystemApplication {
     public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.load();
-
-        // System properties kimi Spring üçün əlavə edirik
-        System.setProperty("DATABASE_URL", dotenv.get("DATABASE_URL"));
-        System.setProperty("DATABASE_USERNAME", dotenv.get("DATABASE_USERNAME"));
-        System.setProperty("DATABASE_PASSWORD", dotenv.get("DATABASE_PASSWORD"));
     SpringApplication.run(HotelManagementSystemApplication.class, args);
     }
     }

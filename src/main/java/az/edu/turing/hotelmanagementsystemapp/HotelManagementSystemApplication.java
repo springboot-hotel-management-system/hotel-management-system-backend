18f278aa2eff65fa52fd73992439d0ea94d3e633
seyran7 <brsr888@gmail.com>
2025-10-18 01:21:24 +0400

    Some changes

--- src/main/java/az/edu/turing/hotelmanagementsystemapp/HotelManagementSystemApplication.java
+++ src/main/java/az/edu/turing/hotelmanagementsystemapp/HotelManagementSystemApplication.java
@@ -1,15 +1,17 @@
 package az.edu.turing.hotelmanagementsystemapp;
 
-
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HotelManagementSystemApplication {
+
     public static void main(String[] args) {
-    SpringApplication.run(HotelManagementSystemApplication.class, args);
-    }
-    }
+        SpringApplication app = new SpringApplication(HotelManagementSystemApplication.class);
 
+        // Local profili aktiv et
+        app.setAdditionalProfiles("local");
 
+        app.run(args);
+    }
+}
--- src/main/java/az/edu/turing/hotelmanagementsystemapp/config/EnvConfig.java
+++ src/main/java/az/edu/turing/hotelmanagementsystemapp/config/EnvConfig.java
@@ -1,16 +1,19 @@
 package az.edu.turing.hotelmanagementsystemapp.config;
 
-
 import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-    public class EnvConfig {
+public class EnvConfig {
 
-        @Bean
-        public Dotenv dotenv() {
-            return Dotenv.load();
-        }
+    @Bean
+    @ConditionalOnProperty(name = "spring.profiles.active", havingValue = "local", matchIfMissing = true)
+    public Dotenv dotenv() {
+        return Dotenv.configure()
+                .ignoreIfMalformed()
+                .ignoreIfMissing()
+                .load();
     }
-
+}
